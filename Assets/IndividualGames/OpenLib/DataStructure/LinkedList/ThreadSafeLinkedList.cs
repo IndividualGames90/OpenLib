299f8e5f4835d2e708d5b64c06e2ae1b3acdcded
IndividualGames90 <IndividualGames@yandex.com>
2023-10-12 14:17:37 -0700

    comments

---
+++ Assets/IndividualGames/OpenLib/DataStructure/LinkedList/ThreadSafeLinkedList.cs
@@ -0,0 +1,74 @@
+﻿namespace IndividualGames.OpenLib.DataStructure.LinkedList
+{
+    /// <summary>
+    /// Thread safe LinkedList for parallel processing.
+    /// </summary>
+    public class ThreadSafeLinkedList<T> : LinkedList<T>
+    {
+        private readonly object _lock = new object();
+
+        public override void AddLast(T value)
+        {
+            lock (_lock)
+            {
+                base.AddLast(value);
+            }
+        }
+
+        public override void AddFirst(T value)
+        {
+            lock (_lock)
+            {
+                base.AddFirst(value);
+            }
+        }
+
+        public override void RemoveFirst()
+        {
+            lock (_lock)
+            {
+                base.RemoveFirst();
+            }
+        }
+
+        public override void RemoveLast()
+        {
+            lock (_lock)
+            {
+                base.RemoveLast();
+            }
+        }
+
+        public override void RemoveFirstOccurance(T value)
+        {
+            lock (_lock)
+            {
+                base.RemoveFirstOccurance(value);
+            }
+        }
+
+        public override bool Contains(T value)
+        {
+            lock (_lock)
+            {
+                return base.Contains(value);
+            }
+        }
+
+        public override bool IsEmpty()
+        {
+            lock (_lock)
+            {
+                return base.IsEmpty();
+            }
+        }
+
+        public override void Clear()
+        {
+            lock (_lock)
+            {
+                base.Clear();
+            }
+        }
+    }
+}
