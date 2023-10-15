a894748723eff8404babcf4e56f84796cdb3ec56
IndividualGames90 <IndividualGames@yandex.com>
2023-10-15 05:23:01 -0700

    queue and stack done

---
+++ Assets/IndividualGames/OpenLib/DataStructure/LinkedList/Queue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace IndividualGames.OpenLib.DataStructure.LinkedList
+{
+    /// <summary>
+    /// Queue utilizing LinkedList.
+    /// </summary>
+    public class Queue<T>
+    {
+        /// <summary> Total current number of elements. </summary>
+        public int Count
+        {
+            get
+            {
+                return linkedList.Count;
+            }
+        }
+
+        private LinkedList<T> linkedList = new();
+
+        public void Enqueue(T value)
+        {
+            linkedList.AddFirst(value);
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            var returnValue = linkedList.Tail;
+            linkedList.RemoveLast();
+            return returnValue;
+        }
+
+        /// <summary> Front of the queue. </summary>
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return linkedList.Tail;
+        }
+
+        public bool IsEmpty()
+        {
+            return linkedList.IsEmpty();
+        }
+
+        public void Clear()
+        {
+            linkedList.Clear();
+        }
+    }
+}
---
+++ Assets/IndividualGames/OpenLib/DataStructure/LinkedList/Stack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace IndividualGames.OpenLib.DataStructure.LinkedList
+{
+    /// <summary>
+    /// Stack utilizing LinkedList.
+    /// </summary>
+    public class Stack<T>
+    {
+        /// <summary> Total current number of elements. </summary>
+        public int Count
+        {
+            get
+            {
+                return linkedList.Count;
+            }
+        }
+
+        private LinkedList<T> linkedList = new();
+
+        public void Push(T value)
+        {
+            linkedList.AddFirst(value);
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            var returnValue = linkedList.Head;
+            linkedList.RemoveFirst();
+            return returnValue;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return linkedList.Head;
+        }
+
+        public bool IsEmpty()
+        {
+            return linkedList.IsEmpty();
+        }
+
+        public void Clear()
+        {
+            linkedList.Clear();
+        }
+    }
+}
