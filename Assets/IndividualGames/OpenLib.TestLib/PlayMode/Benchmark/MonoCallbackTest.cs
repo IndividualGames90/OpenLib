67fba773f4c2d4c925a941ea902f94ee47bebd21
IndividualGames90 <IndividualGames@yandex.com>
2023-11-12 22:39:50 +0300

    mono callbacks tested

---
+++ Assets/IndividualGames/OpenLib.TestLib/PlayMode/Benchmark/MonoCallbackTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace IndividualGames.OpenLib.TestLib.PlayMode.Benchmark
+{
+    /// <summary>
+    /// Add to scene and crank up the amount. Then play and profile.
+    /// </summary>
+    public class MonoCallbackTest : MonoBehaviour
+    {
+        [SerializeField] private int _amountOfMonos;
+        [SerializeField] private GameObject _withCallbacks;
+        [SerializeField] private GameObject _withoutCallbacks;
+
+        private void Awake()
+        {
+            StartCoroutine(StartTest());
+        }
+
+        private IEnumerator StartTest()
+        {
+            yield return new WaitForSeconds(3);
+            Debug.Log("With Callbacks Started");
+            for (int i = 0; i < _amountOfMonos; i++)
+            {
+                Instantiate(_withCallbacks);
+            }
+            yield return new WaitForSeconds(3);
+            Debug.Log("Without Callbacks Started");
+            for (int i = 0; i < _amountOfMonos; i++)
+            {
+                Instantiate(_withoutCallbacks);
+            }
+        }
+    }
+}
---
+++ Assets/IndividualGames/OpenLib.TestLib/PlayMode/Benchmark/WithUnityCallbacks.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace IndividualGames.OpenLib.TestLib.PlayMode.Benchmark
+{
+    public class WithUnityCallbacks : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+        private void Update() { }
+    }
+}
